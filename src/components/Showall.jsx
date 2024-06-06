447a01f95a2c2eb09c373daebd89d98274226698
yiranshu <shuyiran1425@hotmail.com>
2024-06-06 13:38:58 -0400

    routes

--- src/components/Showall.jsx
+++ src/components/Showall.jsx
@@ -4,7 +4,7 @@ export default function Showall (){
     return (
         <>
         <div className="main_inner" >
-         <h2>Show All page</h2>
+             <h2>Show All page</h2>
          </div>
       
         </>
