3cb247608f9d8a21455039a7df52cc2e250c6261
yiranshu <shuyiran1425@hotmail.com>
2024-07-12 12:27:07 -0400

    vh

--- src/components/BlockById.jsx
+++ src/components/BlockById.jsx
@@ -14,13 +14,47 @@ export default function BlockById(){
     },[])
     console.log("blockbyid cover_img: ", block.cover_img)
 
+    //  // Convert newline characters to <br> tags only, no indent
+    // const convertNewlinesToBreaks = (text) => {
+    //     return text ? text.replace(/\n/g, '<br><br>') : '';
+    // };
+
+
+    // Function to convert newline characters to <br> tags and indent first line of each paragraph
+    const convertAndIndent = (text) => {
+        if (!text) return '';
+
+        // Split text into paragraphs
+        const paragraphs = text.split('\n');
+
+        // Process each paragraph
+        return paragraphs.map((paragraph, index) => (
+            <p key={index}>
+                {index === 0 ? (
+                    <span style={{ display: 'inline-block', marginLeft: '2em' }}>{paragraph}</span>
+                ) : (
+                    <span>{paragraph}</span>
+                )}
+            </p>
+        ))
+    }
+
     return (
         <div>
 
-            <img className = "cover_img" src={`/assets/${block.cover_img}`} alt={block.name}/>
-            <br/>
-            <span className = "words"/>{block.words}<span/>
-     
+           
+            {/* {block.cover_img && (
+                <img className="cover_img" src={`/assets/${block.cover_img}`} alt={block.name} />
+            )} */}
+            <br />
+            {/* {block.words && (
+                <div
+                    className="words"
+                    dangerouslySetInnerHTML={{ __html: convertNewlinesToBreaks(block.words) }}
+                />
+            )} */}
+            {block.words && convertAndIndent(block.words)}
+            
     
         </div>
     )
